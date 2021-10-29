37efa9ac88bae9ba85f830ec85c2b0e58641014f
Victor Luz <85527775+vitinop@users.noreply.github.com>
2021-10-29 20:48:41 -0300

    Update users.service.ts

--- aula1-3-5/netflix-clone-master/server/src/users/users.service.ts
+++ aula1-3-5/netflix-clone-master/server/src/users/users.service.ts
@@ -1,12 +1,11 @@
-import { INestApplication, Injectable, ConflictException } from '@nestjs/common';
+import { Injectable, ConflictException } from '@nestjs/common';
 import { Prisma, User } from '@prisma/client';
 import { PrismaService } from 'src/prisma.service';
 import { UserRole } from './enum/role.enum';
 
 @Injectable()
 export class UserService {
-  getHello(): string {
-    return 'Hello Dev!';
+    constructor (private db: PrismaService){}
   }
-}
+
 
