780cb7d987db5a5a76ea13aedb83734f93508496
Victor Luz <85527775+vitinop@users.noreply.github.com>
2021-11-03 16:13:24 -0300

    Validação de busca de usuário por ID, e Buscar vários users

--- aula1-3-5/netflix-clone-master/server/src/users/users.service.ts
+++ aula1-3-5/netflix-clone-master/server/src/users/users.service.ts
@@ -1,16 +1,21 @@
-import { Injectable, ConflictException } from '@nestjs/common';
+import {
+  Injectable,
+  ConflictException,
+  NotFoundException,
+} from '@nestjs/common';
 import { Prisma, User } from '@prisma/client';
 import { PrismaService } from 'src/prisma.service';
 import { UserRole } from './enum/role.enum';
 import * as bcrypt from 'bcrypt';
+import { identity } from 'node_modules/rxjs/dist/types';
+import { removeAllListeners } from 'process';
 
 @Injectable()
 export class UsersService {
   constructor(private db: PrismaService) {}
 
   async create(data: Prisma.UserCreateInput, role: UserRole): Promise<User> {
-    const userExists = await this.db.user.findUnique;
-    ({
+    const userExists = await this.db.user.findUnique({
       where: { email: data.email },
     });
 
@@ -27,8 +32,27 @@ export class UsersService {
         password: hashedPassword,
       },
     });
-    delete user.password;
 
+    delete user.password;
     return user;
+  }
+
+  async findOne(id: string): Promise<User> {
+    const user = await this.db.user.findUnique({
+      where: { id },
+    });
+
+    if (!user) {
+      throw new NotFoundException('ID Não encontrado na base de dados');
+    }
+
+    delete user.password;
+    return user;
+  }
+
+  async findMany() {
+    const user = await this.db.user.findMany();
+    const newUser = user.map(({ password, ...resto }) => resto);
+    return newUser;
   }
 }
