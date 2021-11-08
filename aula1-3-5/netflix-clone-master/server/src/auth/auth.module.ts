b87308aa3ae6d5764c11cc569e01014f7adf3d50
Victor Luz <85527775+vitinop@users.noreply.github.com>
2021-11-08 13:02:23 -0300

    updates de autenticação

--- aula1-3-5/netflix-clone-master/server/src/auth/auth.module.ts
+++ aula1-3-5/netflix-clone-master/server/src/auth/auth.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 import { PrismaService } from 'src/prisma.service';
-import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
-
+import { AuthController } from './auth.controller';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
 @Module({
+  imports: [
+    PassportModule,
+    JwtModule.register({
+      secret: 'kEGfrekjfRRR98TGRu39ERG8ur3k',
+      signOptions: { expiresIn: '1h' },
+    }),
+  ],
   providers: [PrismaService, AuthService],
   controllers: [AuthController],
 })
--- aula1-3-5/netflix-clone-master/server/src/users/users.controller.ts
+++ aula1-3-5/netflix-clone-master/server/src/users/users.controller.ts
@@ -6,11 +6,13 @@ import {
   Param,
   UnprocessableEntityException,
   Delete,
+  UseGuards,
 } from '@nestjs/common';
 import { User } from '@prisma/client';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UsersService } from './users.service';
 import { UserRole } from './enum/role.enum';
+import { SimpleGuard } from 'src/auth/simple.guard';
 
 @Controller()
 export class UsersController {
@@ -21,7 +23,7 @@ export class UsersController {
     delete data.passwordConfirmation;
     return this.service.create(data, UserRole.USER);
   }
-
+  @UseGuards(SimpleGuard)
   @Post('create-admin')
   createAdmin(@Body() data: CreateUserDto): Promise<User> {
     delete data.passwordConfirmation;
