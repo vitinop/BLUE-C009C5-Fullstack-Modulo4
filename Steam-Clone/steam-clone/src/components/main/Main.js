2c02a3d8306dde0906913a59c75bf20afb8b56b3
Victor Luz <85527775+vitinop@users.noreply.github.com>
2021-11-23 04:13:43 -0300

    updates

---
+++ Steam-Clone/steam-clone/src/components/main/Main.js
@@ -0,0 +1,16 @@
+import './Main.css';
+import Header from '../header/Header';
+import Footer from '../foooter/Footer';
+
+function Main() {
+  return ( 
+    <div>
+      <Header></Header>
+    <div className="Main">
+      <Footer></Footer>
+    </div>
+    </div>
+  );
+}
+
+export default Main;
